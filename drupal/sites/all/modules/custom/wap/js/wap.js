d8a46d26178d5fa7dcd49acb513f2627d2c40572
ronn <ronn.abueg@gmail.com>
2013-04-16 17:22:37 -0700

    sample custom code

--- drupal/sites/all/modules/custom/wap/js/wap.js
+++ drupal/sites/all/modules/custom/wap/js/wap.js
@@ -5,23 +5,42 @@
     $('form', context).each(function () { // check for empty required fields
       var form = $(this);
       form.submit(function () {
+        var setError = function () {
+          $('#messages').remove();
+          form.prepend("<div class='messages error'><ul><li>One or more required fields are empty.</li></ul></div>");
+          return false;
+        };
+        var ecnt = 0;
         var required = $('.required', form);
         // reset
         $('.messages.error', form).remove();
         $('.error', form).removeClass('error');
+        
         if (required.length) {
-          var ecnt = 0;
           required.each(function (i,e) {
             if (e.value == '') {
               $(e).addClass('error');
               ecnt++;
             }
           });
-          if (ecnt) {
-            $('#messages').remove();
-            form.prepend("<div class='messages error'><ul><li>One or more required fields are empty.</li></ul></div>");
-          }
         }
+        form.find('.form-radios').each(function () {
+          var options = $(this).find('.form-radio');
+          var hasChecked = options.length==0;
+          options.each(function() {
+            if ($(this).attr('checked')) {
+              hasChecked = true;
+            }
+          });
+          if (!hasChecked) {
+            ecnt++;
+          }
+        });
+        
+        if (ecnt) {
+          return setError();
+        }
+        return true;
       });
     })
   };
