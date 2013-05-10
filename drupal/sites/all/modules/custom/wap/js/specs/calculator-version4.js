c445d68225388e250c35aab14068be48bad776ed
ronn <ronn.abueg@gmail.com>
2013-05-10 15:12:37 -0700

    format

--- drupal/sites/all/modules/custom/wap/js/specs/calculator-version4.js
+++ drupal/sites/all/modules/custom/wap/js/specs/calculator-version4.js
@@ -1,68 +1,68 @@
 describe("Calculator", function() {
-    var calculator;
+  var calculator;
     
-    beforeEach(function() {
-        calculator = new Calculator();
-    });
+  beforeEach(function() {
+    calculator = new Calculator();
+  });
     
-    it("should be able to add two numbers together", function() {
-        var x = 10;
-        var y = 15;
+  it("should be able to add two numbers together", function() {
+    var x = 10;
+    var y = 15;
         
-        calculator.set(x, y);
-        var z = calculator.calculate('+');
+    calculator.set(x, y);
+    var z = calculator.calculate('+');
         
-        expect(z).toEqual(25);
+    expect(z).toEqual(25);
         
-    });
-    
-    
-    it("should be able to subtract from a number", function() {
-        var x = 10;
-        var y = 3;
-        
-        calculator.set(x, y);
-        var z = calculator.calculate('-');
-        
-        expect(z).toEqual(7);
-    });
+  });
     
     
-    it("should be able to multiply two numbers", function() {
-        var x = 5;
-        var y = 6;
+  it("should be able to subtract from a number", function() {
+    var x = 10;
+    var y = 3;
         
-        calculator.set(x, y);
-        var z = calculator.calculate('*');
+    calculator.set(x, y);
+    var z = calculator.calculate('-');
         
-        expect(z).toEqual(30);
-    });
+    expect(z).toEqual(7);
+  });
     
     
-    it("should be able to divide by a number", function() {
-        var x = 60;
-        var y = 12;
+  it("should be able to multiply two numbers", function() {
+    var x = 5;
+    var y = 6;
         
-        calculator.set(x, y);
-        var z = calculator.calculate('/');
+    calculator.set(x, y);
+    var z = calculator.calculate('*');
         
-        expect(z).toEqual(5);
-    });
+    expect(z).toEqual(30);
+  });
     
-    describe("Spies", function() {
+    
+  it("should be able to divide by a number", function() {
+    var x = 60;
+    var y = 12;
+        
+    calculator.set(x, y);
+    var z = calculator.calculate('/');
+        
+    expect(z).toEqual(5);
+  });
+    
+  describe("Spies", function() {
           
-      it("should be able to call add() from calculate()", function() {
-          spyOn(calculator, 'add');
+    it("should be able to call add() from calculate()", function() {
+      spyOn(calculator, 'add');
         
-          var x = 10;
-          var y = 15;
+      var x = 10;
+      var y = 15;
 
-          calculator.set(x, y);
-          var z = calculator.calculate('+');
+      calculator.set(x, y);
+      var z = calculator.calculate('+');
 
-          expect(calculator.add).toHaveBeenCalled();
+      expect(calculator.add).toHaveBeenCalled();
 
-      });
     });
+  });
     
 });
